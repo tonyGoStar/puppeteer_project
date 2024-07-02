a7746b44c2452540326aa009e5a5983afb896d03
Антоний Логинов <aloginov@tennisi.it>
2024-07-02 09:33:30 +0300

    Small changes

--- src/auth.spec.ts
+++ src/auth.spec.ts
@@ -1,12 +1,24 @@
 import puppeteer from "puppeteer";
+// import dotenv from "dotenv";
 
 (async () => {
-    const browsew = await puppeteer.launch({
+    const browser = await puppeteer.launch({
         headless: false,
-        defaultViewport: null,
-        product: 'firefox'
+        defaultViewport: null
+        // product: 'firefox'
     });
-    const page = await browsew.newPage();
+    const page = await browser.newPage();
     await page.goto('https://www.saucedemo.com/')
+    await page.waitForSelector('input[id="user-name"]')
+    await page.type('input[id="user-name"]', 'standard_user')
+    // await page.waitForSelector('input[id="password"]')
+    await page.keyboard.down('Tab')
+    await page.keyboard.type('secret_sauce')
+    
+    await page.keyboard.down('Enter')
+    const prices = await page.$$('data-test="inventory-item-price"')
+    console.log(prices)
+    browser.close()
+    
 
 })()
